891c21b7e1cfcb6b7df8472abc13f14229958760
Tauqeer Murtaza <tauqeer.287@hotmail.com>
2023-10-31 00:11:48 -0500

    Adding ZigZag Solution.

---
+++ 6. Zigzag Conversion/6. Zigzag Conversion/Program.cs
@@ -0,0 +1,9 @@
+﻿using _6._Zigzag_Conversion;
+
+ZigZag zigZag = new ZigZag();
+//PAHNAPLSIIGYIR
+Console.WriteLine(zigZag.Convert("PAYPALISHIRING", 3));
+//PINALSIGYAHRPI
+Console.WriteLine(zigZag.Convert("PAYPALISHIRING", 4));
+//A
+Console.WriteLine(zigZag.Convert("A", 1));
---
+++ 6. Zigzag Conversion/6. Zigzag Conversion/ZigZag.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._Zigzag_Conversion
+{
+    public class ZigZag
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (numRows <= 1)
+                return s;
+            string[] ans = new string[numRows];
+            int count = 0;
+            bool goingUp = false;
+            for (int i = 0; i < s.Length; i++)
+            {
+                ans[count] += s[i];
+                if (goingUp)
+                {
+                    count--;
+                    if(count == 0)
+                        goingUp = false;
+                }
+                else
+                {
+                    count++;
+                    if (count == numRows - 1)
+                        goingUp = true;
+                }
+            }
+
+            StringBuilder str = new StringBuilder();
+            foreach (var word in ans)
+            {
+                str.Append(word);
+            }
+            return str.ToString();
+        }
+    }
+}
