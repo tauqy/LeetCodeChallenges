889ac24a79d8ae4a413bff5420a88d80aa4451e2
Tauqeer Murtaza <tauqeer.287@hotmail.com>
2023-11-02 21:10:08 -0500

    Adding solution for Leetcode Issue#1441

---
+++ 1441. Build an Array With Stack Operations/1441. Build an Array With Stack Operations/Program.cs
@@ -0,0 +1,39 @@
+﻿using _1441._Build_an_Array_With_Stack_Operations;
+
+/*
+ * Example 1:
+
+Input: target = [1,3], n = 3
+Output: ["Push","Push","Pop","Push"]
+Explanation: Initially the stack s is empty. The last element is the top of the stack.
+Read 1 from the stream and push it to the stack. s = [1].
+Read 2 from the stream and push it to the stack. s = [1,2].
+Pop the integer on the top of the stack. s = [1].
+Read 3 from the stream and push it to the stack. s = [1,3].
+Example 2:
+
+Input: target = [1,2,3], n = 3
+Output: ["Push","Push","Push"]
+Explanation: Initially the stack s is empty. The last element is the top of the stack.
+Read 1 from the stream and push it to the stack. s = [1].
+Read 2 from the stream and push it to the stack. s = [1,2].
+Read 3 from the stream and push it to the stack. s = [1,2,3].
+Example 3:
+
+Input: target = [1,2], n = 4
+Output: ["Push","Push"]
+Explanation: Initially the stack s is empty. The last element is the top of the stack.
+Read 1 from the stream and push it to the stack. s = [1].
+Read 2 from the stream and push it to the stack. s = [1,2].
+Since the stack (from the bottom to the top) is equal to target, we stop the stack operations.
+The answers that read integer 3 from the stream are not accepted.
+ */
+
+StackClass stack = new StackClass();
+var arr = new int[] { 1,2,3 };
+var res = stack.BuildArray(arr, 4);
+
+foreach (var item in res)
+{
+    Console.WriteLine($"{item},");
+}
---
+++ 1441. Build an Array With Stack Operations/1441. Build an Array With Stack Operations/StackClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1441._Build_an_Array_With_Stack_Operations
+{
+    public class StackClass
+    {
+        public IList<string> BuildArray(int[] target, int n)
+        {
+            var list = new List<string>();
+            var stack = new Stack<int>();
+            int count = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                if (target.Length > count) {
+                    stack.Push(i + 1);
+                    list.Add("Push");
+                    if ( stack.Peek() != target[count++])
+                    {
+                        stack.Pop();
+                        list.Add("Pop");
+                        count--;
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
